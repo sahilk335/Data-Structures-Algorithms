5bc528930de912acb4db5ac8b31b9496ce73ac03
sahilk335 <sahilk335@gmail.com>
2018-02-22 08:24:57 +0900

    Random Node add

--- src/LinkedList/Node.java
+++ src/LinkedList/Node.java
@@ -3,9 +3,11 @@ package LinkedList;
 public class Node {
     int data;
     Node next;
+    Node random;
 
     Node(int data){
         this.data=data;
         this.next=null;
+        this.random=null;
     }
 }
