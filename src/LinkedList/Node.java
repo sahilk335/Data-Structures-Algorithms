cd8de919608a3000f1cc7c609626daa49ca07770
sahilk335 <sahilk335@gmail.com>
2018-02-27 00:21:28 +0900

    LRU CACHE

--- src/LinkedList/Node.java
+++ src/LinkedList/Node.java
@@ -4,10 +4,17 @@ public class Node {
     int data;
     Node next;
     Node random;
+    Node prev;
+    int key;
 
     Node(int data){
         this.data=data;
         this.next=null;
         this.random=null;
+    }
+
+    Node(int key,int data){
+        this.key=key;
+        this.data=data;
     }
 }
