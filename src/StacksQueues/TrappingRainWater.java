98edbfb024dea5dc46ee79fe0bb3a7e8dcc9b712
sahilk335 <sahilk335@gmail.com>
2019-05-18 06:53:16 +0530

    Trapping Rain Water using 2 pointers

--- src/StacksQueues/TrappingRainWater.java
+++ src/StacksQueues/TrappingRainWater.java
@@ -77,6 +77,26 @@ public class TrappingRainWater {
         return answer;
     }
 
+    //Using 2 pointer
+    int trappingWater(int height[]) {
+        int a = 0;
+        int b = height.length - 1;
+        int max = 0;
+        int leftmax = 0;
+        int rightmax = 0;
+        while (a <= b) {
+            leftmax = Math.max(leftmax, height[a]);
+            rightmax = Math.max(rightmax, height[b]);
+            if (leftmax < rightmax) {
+                max += (leftmax - height[a]);       // leftmax is smaller than rightmax, so the (leftmax-A[a]) water can be stored
+                a++;
+            } else {
+                max += (rightmax - height[b]);
+                b--;
+            }
+        }
+        return max;
+    }
     public static void main(String args[]) {
         int arr[] = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         TrappingRainWater trappingRainWater = new TrappingRainWater();
