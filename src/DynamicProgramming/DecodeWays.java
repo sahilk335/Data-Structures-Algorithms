98b5d816b90d7bb057eb1d5252d3764a8a153308
sahilk335 <sahilk335@gmail.com>
2019-06-14 10:05:11 +0530

    Decode Ways

---
+++ src/DynamicProgramming/DecodeWays.java
@@ -0,0 +1,62 @@
+package DynamicProgramming;
+
+/*
+ *@Author : Sahil Khurana
+ * Date : 14 June 2019
+ *
+ * A message containing letters from A-Z is being encoded to numbers using the following mapping:
+ *
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ * Given a non-empty string containing only digits, determine the total number of ways to decode it.
+ *
+ * Example 1:
+ *
+ * Input: "12"
+ * Output: 2
+ * Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+ * Example 2:
+ *
+ * Input: "226"
+ * Output: 3
+ * Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+ *
+ * References : https://leetcode.com/problems/decode-ways/
+ *
+ * Solution :
+ * 1. dp[0] means an empty string will have one way to decode
+ * 2. dp[1] means the way to decode a string of size 1
+ * 3. check one digit and two digit combination and save the results along the way
+ * 4.dp[n] will be the end result
+ *
+ */
+public class DecodeWays {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i - 1, i));
+            int second = Integer.valueOf(s.substring(i - 2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i - 1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        DecodeWays decodeWays = new DecodeWays();
+        System.out.println(decodeWays.numDecodings("101"));
+    }
+}
